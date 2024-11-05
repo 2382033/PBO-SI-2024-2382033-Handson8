ee1987874c4c346b036671d9f3fe9186969a51e7
2382033 <2382033@unai.edu>
2024-11-05 10:49:31 +0700

    HANDSON8

---
+++ services/TodoListServices.java
@@ -0,0 +1,10 @@
+package services;
+
+import Entities.TodoList;
+
+public interface TodoListServices {
+    TodoList[] getTodoList();
+    void addTodoList(String todo);
+    Boolean removeTodoList(Integer number);
+    Boolean editTodoList(Integer number, String todo);
+}
